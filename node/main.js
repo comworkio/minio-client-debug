927d6e8cc30558f43c9f14e3056bea8045fa48d5
Idriss Neumann <idriss.neumann@comwork.io>
2023-02-28 14:00:38 +0100

    Fix: fput with md5 hash

--- node/main.js
+++ node/main.js
@@ -1,5 +1,6 @@
 import * as Minio from 'minio';
-import { writeFile } from 'fs';
+import * as crypto from 'crypto';
+import { writeFile, createReadStream } from 'fs';
 import { setTimeout } from 'timers/promises';
 
 var filename = process.env.BUCKET_FILENAME
@@ -7,25 +8,30 @@ var wait_time = process.env.WAIT_TIME
 
 var minioClient = new Minio.Client({
     endPoint: process.env.BUCKET_ENDPOINT,
-    port: 443,
-    useSSL: true,
     accessKey: process.env.BUCKET_ACCESS_KEY,
-    secretKey: process.env.BUCKET_SECRET_KEY
+    secretKey: process.env.BUCKET_SECRET_KEY,
+    region: process.env.BUCKET_REGION
 });
 
 while(true) {
     var line = "Written at " + new Date().toISOString()
     writeFile(filename, line, { encoding: "utf8", flag: "w" }, function (){})
-
-    var metaData = {
-        'Content-Type': 'application/octet-stream'
-    }
-
-    minioClient.fPutObject(process.env.BUCKET_NAME, process.env.BUCKET_REGION, filename, metaData, function(err, etag) {
-        if (err) {
-            return console.log(err)
-        }
-    });
+    var hash = crypto.createHash('md5');
+    hash.setEncoding('base64');
+    var fileStream = createReadStream(filename);
+    fileStream.on('end', () => {
+        hash.end();
+        minioClient.fPutObject(process.env.BUCKET_NAME, filename, "dir/" + filename, { 
+                'Content-Type': 'application/octet-stream', 
+                'Content-MD5': hash.read() 
+            }, 
+            function(err, etag) {
+                if (err) {
+                    return console.log(err)
+                }
+            }
+        );
+    })
 
     console.log(line)
     await setTimeout(wait_time * 1000);
